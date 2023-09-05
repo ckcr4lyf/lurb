21aaebe0300280dbe9c230d33512304967421b2b
Raghu Saxena <poiasdpoiasd@live.com>
2023-09-05 22:27:00 +0800

    Base CLI

---
+++ bin/index.mjs
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+import { Command } from 'commander';
+import { Logger, LOGLEVEL } from '@ckcr4lyf/logger';
+
+const logger = new Logger({ loglevel: LOGLEVEL.DEBUG });
+
+const program = new Command();
+
+program.command('connectable').description('Check if your client is connectable')
+.requiredOption('-h, --host <host>', 'Host / IP address of the client')
+.requiredOption('-p, --port <port>', 'Port the client is listening on')
+.action(async (options) => {
+    logger.debug(`Called with host=${options.host}, port=${options.port}`);
+});
